00f8a9c3114a15e0e84c99fd6c9ffff5e040f5f9
André Vennberg <andre.vennberg@gmail.com>
2016-07-07 13:10:31 +0200

    Added TUI and input to server :D

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,23 +1,37 @@
-#include <enet/enet.h>
-#include <ncurses.h>
-
-#include <cstdio>
 #include <map>
+#include <chrono>
+#include <cstdio>
+#include <string>
+#include <thread>
+#include <iostream>
+
+#include <ncurses.h>
+#include <enet/enet.h>
 
 #include <json.hpp>
-
-#include "Input.h"
-#include "Log.h"
 
 static bool Verbose = false;
 
 static ENetAddress address;
 static ENetHost* server;
 
-static Input input;
-Log messageLog;
-
 static std::map<enet_uint32, ENetPeer*> ConnectedPeers;
+
+static bool Exit = false;
+static bool BlockingIO = false;
+
+static int MaxX;
+static int MaxY;
+
+static int TopLine = 1;
+
+static WINDOW* Top;
+static WINDOW* Bottom;
+
+static enet_uint32 Host = ENET_PORT_ANY;
+static int Port = 1234;
+
+void Write(std::string str, ...);
 
 std::vector<std::string> Split(const std::string &s, const char delim) {
     std::vector<std::string> elements;
@@ -38,17 +52,8 @@ void Init_Connection(enet_uint32 host, int port) {
     address.port = static_cast<unsigned short>(port);
 
     server = enet_host_create(&address, 32, 2, 0, 0);
-
-    puts("SERVER READY\n");
-}
-
-void Init_Curses() {
-    initscr();
-    raw();
-    noecho();
-
-    input.Init();
-    messageLog.Init();
+    
+    Write("Ready!");
 }
 
 void Send(enet_uint32 peerID, std::string message, int channel) {
@@ -61,20 +66,33 @@ void Broadcast(std::string message, int channel) {
     enet_host_broadcast(server, static_cast<unsigned char>(channel), packet);
 }
 
-int main(int argc, char* argv[]) {
-    Init_Curses();
+void Init_Curses() {
+    initscr();
+    
+    getmaxyx(stdscr, MaxY, MaxX);
 
-    enet_uint32 host = ENET_PORT_ANY;
-    int port = 1234;
+    Top = newwin(MaxY - 3, MaxX, 0, 0);
+    Bottom = newwin(3, MaxX, MaxY - 3, 0);
 
-    for (int i = 1; i < argc; ++i) {
-        std::string argument(argv[i]);
+    scrollok(Top, TRUE);
+    scrollok(Bottom, TRUE);
+
+    box(Top, '|', '=');
+    box(Bottom, '|', '-');
+
+    wsetscrreg(Top, 1, MaxY - 5);
+    wsetscrreg(Bottom, 1, 1);
+}
+
+void Parse_Arguments(int count, char* args[]) {
+    for (int i = 1; i < count; ++i) {
+        std::string argument(args[i]);
 
         if (argument == "--verbose" || argument == "-v") {
             Verbose = true;
         }
-        else if ((argument == "--host" || argument == "-h") && i + 1 < argc) {
-            std::string addr = argv[++i];
+        else if ((argument == "--host" || argument == "-h") && i + 1 < count) {
+            std::string addr = args[++i];
             std::vector<std::string> ipParts = Split(addr, '.');
             std::stringstream hexRepr;
 
@@ -98,27 +116,96 @@ int main(int argc, char* argv[]) {
 
             std::stringstream ss;
             ss << std::hex << hexRepr.str();
-            ss >> host;
+            ss >> Host;
         }
-        else if ((argument == "--port" || argument == "-p") && i + 1 < argc) {
-            port = std::stoi(argv[++i]);
+        else if ((argument == "--port" || argument == "-p") && i + 1 < count) {
+            Port = std::stoi(args[++i]);
         }
     }
+}
 
-    Init_Connection(host, port);
-
+void Input_Thread() {
+    char str[80];
+    
     while (true) {
-        char pressedKey = static_cast<char>(wgetch(cmdWin));
+        bzero(str, 80);
+        wrefresh(Bottom);
 
-        if (pressedKey != -1) {
-            if (pressedKey == 'q') {
-                break;
-            }
-            else {
-                input.Type(pressedKey);
-            }
+        mvwgetstr(Bottom, 1, 2, str);
+        std::string message(str);
+
+        if (message == "") {
+            continue;
         }
 
+        while (BlockingIO) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+
+        BlockingIO = true;
+
+        mvwprintw(Top, TopLine, 3, message.c_str());
+        wmove(Bottom, 1, 2);
+        wclrtoeol(Bottom);
+
+        if (message == "exit") {
+            Exit = true;
+            return;
+        }
+
+        if (TopLine != (MaxY - 5)) {
+            ++TopLine;
+        }
+        else {
+            scroll(Top);
+            box(Top, '|', '=');
+        }
+
+        wrefresh(Top);
+
+        BlockingIO = false;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
+void Write(std::string str, ...) {
+    while (BlockingIO) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    BlockingIO = true;
+
+    wmove(Top, TopLine, 3);
+
+    va_list args;
+    va_start(args, str);
+    vw_printw(Top, str.c_str(), args);    
+    va_end(args);
+
+    if (TopLine != (MaxY - 5)) {
+        ++TopLine;
+    }
+    else {
+        scroll(Top);
+        box(Top, '|', '=');
+    }
+
+    wrefresh(Top);
+
+    wmove(Bottom, 1, 2);
+
+    BlockingIO = false;
+}
+
+int main(int argc, char* argv[]) {
+    Init_Curses();
+    Parse_Arguments(argc, argv);
+    Init_Connection(Host, Port);
+
+    std::thread input(Input_Thread);
+
+    while (!Exit) {
         ENetEvent event;
 
         while (enet_host_service(server, &event, 50) > 0) {
@@ -127,12 +214,16 @@ int main(int argc, char* argv[]) {
                     break;
 
                 case ENET_EVENT_TYPE_CONNECT:
-                    printf("PEER CONNECTED FROM %x:%u\n", event.peer->address.host, event.peer->address.port);
+                    Write("Peer connected from %x:%u.", event.peer->address.host, event.peer->address.port);
+                    Write("");
+
                     ConnectedPeers[event.peer->connectID] = event.peer;
                     break;
 
                 case ENET_EVENT_TYPE_DISCONNECT:
-                    printf("%s DISCONNECTED\n\n", event.peer->data);
+                    Write("%s DISCONNECTED", event.peer->data);
+                    Write("");
+
                     ConnectedPeers.erase(event.peer->connectID);
                     break;
 
@@ -142,28 +233,38 @@ int main(int argc, char* argv[]) {
                     for (nlohmann::json::iterator it = j["events"].begin(); it != j["events"].end(); ++it) {
                         if (it.key() == "connect") {
                             event.peer->data = const_cast<char*>(it.value()["name"].get<std::string>().c_str());
-
-                            puts("PEER CONNECTED");
-                            printf("NAME: %s\n\n", it.value()["name"].get<std::string>().c_str());
+                            Write("%s connected.", it.value()["name"].get<std::string>().c_str());
                         }
                         else if (it.key() == "blockBreak") {
-                            puts("BLOCK BROKEN");
-                            printf("AT POSITION: %s\n", it.value()["pos"].get<std::string>().c_str());
-                            printf("BY: %s\n\n", it.value()["player"].get<std::string>().c_str());
+                            if (Verbose) {
+                                Write("BLOCK BROKEN");
+                                Write("AT POSITION: %s", it.value()["pos"].get<std::string>().c_str());
+                                Write("BY: %s", it.value()["player"].get<std::string>().c_str());
+                                Write("");
+                            }
 
                             nlohmann::json response;
                             response["events"]["breakBlock"]["pos"] = it.value()["pos"];
                             response["events"]["breakBlock"]["player"] = it.value()["player"];
                             Broadcast(response.dump(), event.channelID);
                         }
+                        else if (it.key() == "message") {
+                            Write("%s: %s", event.peer->data, j["events"]["message"].get<std::string>().c_str());
+
+                            nlohmann::json response;
+                            response["events"]["message"]["value"] = j["events"]["message"];
+                            response["events"]["message"]["player"] = std::string(static_cast<char*>(event.peer->data));
+                            Broadcast(response.dump(), event.channelID);
+                        }
                     }
 
                     if (Verbose) {
-                        puts("PACKET RECEIVED");
-                        printf("LENGTH: %zu\n", event.packet->dataLength);
-                        printf("DATA: %s\n", event.packet->data);
-                        printf("SENDER: %s\n", event.peer->data);
-                        printf("CHANNEL ID: %u\n\n", event.channelID);
+                        Write("PACKET RECEIVED");
+                        Write("LENGTH: %zu", event.packet->dataLength);
+                        Write("DATA: %s", event.packet->data);
+                        Write("SENDER: %s", event.peer->data);
+                        Write("CHANNEL ID: %u", event.channelID);
+                        Write("");
                     }
 
                     break;
@@ -171,8 +272,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    enet_deinitialize();
     endwin();
-
+    input.join();
+    enet_deinitialize();
     return 0;
 }
